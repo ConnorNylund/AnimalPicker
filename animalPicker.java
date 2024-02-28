2b171071a01fddd66b07944d7f5900e3a78b4903
connornylund <csnylund@quinnipiac.edu>
2024-02-28 14:10:21 -0500

    added the art fo cat and dog

--- animalPicker.java
+++ animalPicker.java
@@ -10,15 +10,22 @@ public class animalPicker {
         Scanner scan = new Scanner( System.in );
 
         while (true) {
-            System.out.println("What animal would you like to see: Dog or Cat or Fish?");// Askes the user to enter one of the animal names
+            System.out.println("What animal would you like to see: Dog or Cat?");// Askes the user to enter one of the animal names
             cmd = scan.nextLine();// Takes the user input
 
             switch (cmd.toLowerCase()) {
                 case "dog":
                     System.out.println("DOG ASCII ART HERE");// Prints out dog ASCII art
+                    System.out.println("^..^      /\r\n" + //
+                                       "/_/\\_____/\r\n" + //
+                                       "   /\\   /\\\r\n" + //
+                                       "  /  \\ /  \\");
                     return; 
                 case "cat":
                     System.out.println("CAT ASCII ART HERE");// Prints out cat ASCII art
+                    System.out.print("|\\---/|\r\n"+
+                                        "| o_o |\r\n"+
+                                        " \\_^_/");
                     return; 
                 default:
                     System.out.println("Not an option. Please enter 'dog' or 'cat'."); // Loops around untill you enter a valid option.
