c5bd2651976e5019154f27999aaeb49430fff74b
connornylund <csnylund@quinnipiac.edu>
2024-02-28 14:06:04 -0500

    added loop for selection

--- animalPicker.java
+++ animalPicker.java
@@ -9,20 +9,20 @@ public class animalPicker {
 
         Scanner scan = new Scanner( System.in );
 
-            System.out.println("What animal would you like to see: Dog or Cat?");// Askes the user to enter one of the animal names
+        while (true) {
+            System.out.println("What animal would you like to see: Dog or Cat or Fish?");// Askes the user to enter one of the animal names
             cmd = scan.nextLine();// Takes the user input
 
             switch (cmd.toLowerCase()) {
                 case "dog":
                     System.out.println("DOG ASCII ART HERE");// Prints out dog ASCII art
                     return; 
-                
                 case "cat":
                     System.out.println("CAT ASCII ART HERE");// Prints out cat ASCII art
                     return; 
-                
                 default:
-                    System.out.println("Not an option. Please enter 'dog' or 'cat'."); 
+                    System.out.println("Not an option. Please enter 'dog' or 'cat'."); // Loops around untill you enter a valid option.
+            }
         }
     }
 }
